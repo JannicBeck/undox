314406658b5df7c3258fa9bfc04113be4b26f05f
jannicbeck <jannicbeck@gmail.com>
2018-04-14 00:11:28 +0200

    Remove js files and use react-scripts-ts

--- examples/react/counter-typescript/src/reducers/index.js
+++
@@ -1,5 +0,0 @@
-import { undox, createSelectors } from 'undox';
-import { counter } from './counter';
-import { init } from '../actions';
-export const reducer = undox(counter, init());
-export const selectors = createSelectors(counter);
